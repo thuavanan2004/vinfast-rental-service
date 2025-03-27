e918d9ff3f228f349391ae5fbedb8357a6522c4e
thuavanan2004 <thuavanan2004@gmail.com>
2025-03-27 22:53:00 +0700

    feat: config openApi for swagger docs

--- src/main/java/com/vinfast/rental_service/controllers/AdminController.java
+++ src/main/java/com/vinfast/rental_service/controllers/AdminController.java
@@ -5,12 +5,15 @@ import com.vinfast.rental_service.dtos.request.AdminCreateRequest;
 import com.vinfast.rental_service.dtos.response.AdminDetailResponse;
 import com.vinfast.rental_service.entity.Admin;
 import com.vinfast.rental_service.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Admin")
 @Slf4j
 @RestController
 @RequestMapping("/api/admin/admins")
@@ -18,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final AdminService adminService;
 
-
+    @Operation(summary = "Get account admin")
     @GetMapping("/{id}")
     public ResponseEntity<AdminDetailResponse> getAdmin(@PathVariable Long id) {
         return ResponseEntity.ok(adminService.getAdminById(id));
